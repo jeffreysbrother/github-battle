74553bf79786b62886d210dd82e67d49456fa4fc
James Cool <jeffreysbrother@gmail.com>
2017-03-14 23:34:49 -0700

    wipe out examples

--- app/index.js
+++ app/index.js
@@ -1,17 +1,51 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function() {
+// var HelloWorld = React.createClass({
+//   render: function() {
+//     return (
+//       <div>
+//         <h1>Hello, {this.props.name}</h1>
+//       </div>
+//     )
+//   }
+// });
+//
+// ReactDOM.render(
+//   <HelloWorld name="James" />,
+//   document.getElementById('app')
+// );
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend) {
+      return <li> {friend} </li>;
+    });
     return (
       <div>
-        <h1>Hello, {this.props.name}</h1>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
+var FriendsContainer = React.createClass({
+  render: function() {
+    var name = 'Tyler McGinnis'
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+    return (
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
       </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld name="James" />,
+  <FriendsContainer />,
   document.getElementById('app')
 );
